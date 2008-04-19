bf94c3bd50b096f88d19788bcef0385dd5fa6eaf
Bob Forgey <rforgey@grumpydogconsulting.com>
2008-04-18 23:18:00 -0700

    old changes 04

---
+++ tsts/accept/01GettagsTest.pm
@@ -0,0 +1,66 @@
+package GettagsTest;
+use base qw(Test::Unit::TestCase);
+
+use Benchmark qw(:all);
+use Data::Dumper;
+use File::Basename;
+use Idval;
+use Idval::Common;
+
+use AcceptUtils;
+
+#our $data_dir = $main::topdir . '/' . "tsts/accept_data/ValidateTest";
+
+sub new {
+    my $self = shift()->SUPER::new(@_);
+    # your state for fixture here
+    $self->{IDVAL} = Idval->new({'verbose' => 0,
+                                 'quiet' => 0});
+    $self->{LOG} = Idval::Common::get_logger();
+    return $self;
+}
+
+sub set_up {
+    # provide fixture
+}
+sub tear_down {
+    # clean up after test
+}
+
+sub test_gettags
+{
+    my $self = shift;
+    my $idval = $self->{IDVAL};
+    my $errlist = '';
+
+    my @files = map{ $AcceptUtils::audiodir . "/sbeep$_" } qw(.flac .ogg .mp3);
+    my $taglist = $idval->datastore();
+    $taglist = Idval::Scripts::gettags($taglist, $idval->providers(), $AcceptUtils::audiodir);
+    #$taglist = Idval::Scripts::print($taglist, $idval->providers());
+
+    #print STDERR "Checking: ", @files, "\n";
+    foreach my $file (@files)
+    {
+        if (!$taglist->key_exists($file))
+        {
+            $errlist .= "No tag entries for \"$file\". Tag reader not present?\n";
+        }
+        elsif (!$taglist->get($file)->key_exists("TITLE"))
+        {
+            $errlist .= "no \"TITLE\" entry for $file.\n";
+        }
+        else
+        {
+            my $fname = lc(basename($file));
+            #print STDERR "fname is \"$fname\", tag value is: \"" . $taglist->get($file)->get_value("TITLE") . "\"\n";
+            if ($fname ne $taglist->get($file)->get_value("TITLE"))
+            {
+                $errlist .= "Bad TITLE for \"$file\" (is \"" . $taglist->get($file)->get_value("TITLE") . "\")\n";
+            }
+        }
+    }
+
+    $self->assert(0, $errlist) if $errlist;
+}
+
+1;
