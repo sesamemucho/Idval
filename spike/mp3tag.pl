bd4a7ec675e7a3769ab5fc760c63e3fedff28964
Bob Forgey <rforgey@grumpydogconsulting.com>
2008-05-23 22:25:36 -0700

    old changes 15

--- spike/mp3tag.pl
+++ spike/mp3tag.pl
@@ -1,15 +1,22 @@
-use lib '../../proj/ext/MPEG-ID3v2Tag-0.39/blib/lib';
+use FindBin;
+use lib '$FindBin::Bin/../../proj/ext/MPEG-ID3v2Tag-0.39/lib';
+use lib '$FindBin::Bin/../../proj/ext/MPEG-ID3v2Tag-0.39/lib';
 use MPEG::ID3v2Tag;
 use IO::File;
+use Data::Dumper;
 
 # create a tag
 open(OUTFILE, ">a.bin");
 binmode OUTFILE;
 $tag = MPEG::ID3v2Tag->new();
-$tag->add_frame( "TIT2", "Happy Little Song" );    # one step
-$frame = MPEG::ID3Frame::TALB->new("Happy little album");
+#$tag->add_frame( "TIT2", "Happy Little Song" );    # one step
+$tag->add_frame( "TIT2", "Gak" );    # one step
+#$frame = MPEG::ID3Frame::TALB->new("Happy little album");
+$frame = MPEG::ID3Frame::TALB->new("Glob");
 $tag->add_frame($frame);                           # two steps
 $tag->add_frame( "WCOM", "http://www.mp3.com" );
+$tag->add_frame( "TXXX", 0, "foofah", "hoohah");
+$tag->add_frame( "TXXX", 0, "Googah", "gubbers");
 $tag->set_padding_size(256);
 print OUTFILE $tag->as_string();
 close OUTFILE;
@@ -19,13 +26,16 @@ close OUTFILE;
 $fh = IO::File->new("<a.mp3");
 binmode $fh;
 $tag = MPEG::ID3v2Tag->parse($fh);
+#print "tag: ", Dumper($tag);
 for $frame ( $tag->frames() ) {
-    print $frame->frameid(), "\n";    # prints TALB, TIT2, WCOM, etc.
+    print $frame->frameid(), "\n\n";    # prints TALB, TIT2, WCOM, etc.
     if ( $frame->flag_read_only() ) {
         print "  read only\n";
     }
     if ( $frame->fully_parsed() && $frame->frameid =~ /^T.../ ) {
         print "  frame text is ", $frame->text(), "\n";
+        print " text frame dump:\n";
+        $frame->dump();
     }
     if ( $frame->fully_parsed() && $frame->frameid =~ /^W.../ ) {
         print "  url is ", $frame->url(), "\n";
