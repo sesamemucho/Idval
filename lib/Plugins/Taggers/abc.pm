086a36ac0f1d3576f3e00b0e702f4faa3ccb40f3
Bob Forgey <rforgey@grumpydogconsulting.com>
2008-06-17 23:02:16 -0700

    old changes 26

--- lib/Plugins/Taggers/abc.pm
+++ lib/Plugins/Taggers/abc.pm
@@ -22,6 +22,7 @@ use strict;
 use warnings;
 no  warnings qw(redefine);
 use Class::ISA;
+use Data::Dumper;
 use Carp;
 
 use Idval::FileIO;
@@ -68,7 +69,37 @@ sub read_tags
     my $current_tag;
     my $retval = 0;
 
-    my $filename = $tag_record->get_value('FILE');
+    my $fileid = $tag_record->get_value('FILE');
+    my ($fname, $id) = ($fileid =~ m/^(.*)%%(\d+)/);
+
+    if (!exists($self->{TEXT}->{$fname}))
+    {
+        print "Parsing \"$fname\"\n";
+        my $fh = Idval::FileIO->new($fname, "r") || croak "Can't open \"$fname\" for reading: $!\n";
+        my $text = do { local $/ = undef; <$fh> };
+        $fh->close();
+
+      LOOP:
+        {
+            $self->{TEXT}->{$fname}->{PREFACE} = $1, redo LOOP if $text =~ m/\G(^.*?\n\s*)(?=X:)/gsc;
+            $self->{TEXT}->{$fname}->{BLOCK}->{sprintf("%04d", $2)} = $1, redo LOOP if $text =~ m/\G(X:\s*(\d+).*?)(?=X:|\z)/gsc;
+        }
+
+        #print Dumper($self->{TEXT});
+    }
+    
+    print "Checking block $id\n";
+
+    my $text = $self->{TEXT}->{$fname}->{BLOCK}->{$id};
+  LOOP2:
+    {
+        if ($text =~ m/\G(([ABCDFGHIKLMmNOPQRrSTUVWXZ]):.*?)(?=[ABCDFGHIKLMmNOPQRrSTUVWZ]:|\z)/gsc)
+        {
+            print("Got $2 field\n");
+            redo LOOP2 ;
+        }
+    }
+
 
     return $retval;
     
@@ -143,7 +174,7 @@ sub create_records
     my $srclist = $arglist->{srclist};
 
     my $fh = Idval::FileIO->new($fname, "r") || croak "Can't open \"$fname\" for reading: $!\n";
-    my $text .= do { local $/ = undef; <$fh> };
+    my $text = do { local $/ = undef; <$fh> };
     $fh->close();
 
     my $item;
@@ -153,7 +184,7 @@ sub create_records
         $line =~ m/^X:(\d+)/x and do {
             $item = $1;
 
-            my $rec = Idval::Record->new($path . '%%' . $item);
+            my $rec = Idval::Record->new(sprintf("%s%04d", $path . '%%', $item));
             $rec->add_tag('CLASS', $class);
             $rec->add_tag('TYPE', $type);
 
